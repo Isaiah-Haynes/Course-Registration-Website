1d6af933dbe0e478e656e5b4751cd31a1b75fba0
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2023-10-09 09:49:03 -0400

    trying to upload vueApp again

---
+++ crsProject/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="home">
+    <h2>The Home Page</h2>
+    <p>
+      This application includes this home page, a page that fetches data, and a
+      page that handles state in a form.
+    </p>
+    <p>
+      These pages, along with the structure of the project, are designed to help
+      you get started solving similar technical challenges, based on the
+      features you are looking to add to your application.
+    </p>
+  </main>
+</template>
+
+<style>
+.home {
+  padding: 1rem;
+}
+
+.home h2 {
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+}
+
+.home p {
+  margin-bottom: 1rem;
+}
+</style>
