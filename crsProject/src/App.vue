3d8477ffdc134c801d65317cfa80aca83a8de11e
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2023-10-16 10:30:09 -0400

    changed directory name

---
+++ crsProject/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app-wrapper">
+      <AppHeader title="Course Registration Template" />
+      <RouterView />
+    </div>
+  </template>
+  
+  <script setup>
+  import { RouterView } from "vue-router";
+  import AppHeader from "./components/AppHeader.vue";
+  </script>
