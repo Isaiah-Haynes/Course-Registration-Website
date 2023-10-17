423efdef6a3c893c5326a4a482f87f8ce335cc17
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2023-10-17 16:37:34 -0400

    added comments

--- amazonServices/dynamoDB/dynamoAddMultiple.py
+++ amazonServices/dynamoDB/dynamoAddMultiple.py
@@ -6,6 +6,7 @@ import json
 import boto3
 import sys
 
+#checks that there is an argument given when file is run
 def check_args():
     if len(sys.argv) == 2:
         return 1
@@ -17,6 +18,7 @@ def batch_put(courseCatalog):
     table = DDB.Table('courseCatalog')
     with table.batch_writer() as batch:
         for course in courseCatalog:
+            #give variable names to the items in json file
             course_name = course['course_name']
             num_credits = course['num_credits']
             course_title = course['course_title']
@@ -24,6 +26,8 @@ def batch_put(courseCatalog):
             course_end = course['course_end']
             course_days = course['course_days']
             subject= course['subject']
+            
+            #formats the data read back to json to put in table
             formatted_data  = {
                 'course_name': course_name,
                 'num_credits': num_credits,
@@ -36,16 +40,20 @@ def batch_put(courseCatalog):
 
             print("Adding course:", course_name, course_title)
 
+            #puts course in dynamoDB table
             batch.put_item(Item=formatted_data)
-    #return "Adding course: "+course_name
 
    
 if __name__ == '__main__':
+
+    #checks that the correct argument is given when file is run
     if check_args() and ".json" in sys.argv[1]:
         inputFile = sys.argv[1]
     else:
         print("Usage: python3 dynamoAddMultiple.py <input json file>")
         exit()
+    
+    #opens json file from given argument
     with open(inputFile) as json_file:
         courseCatalog = json.load(json_file)['courseCatalog_arr']
     batch_put(courseCatalog)
