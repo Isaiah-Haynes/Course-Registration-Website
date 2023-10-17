605405e88c4335082a085ae12f0744ae85fa068a
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2023-10-16 20:44:58 -0400

    updated dynamoAddMultiple_test.py

--- amazonServices/dynamoDB/dynamoAddMultiple_test.py
+++ amazonServices/dynamoDB/dynamoAddMultiple_test.py
@@ -1,15 +1,8 @@
 import json
 from dynamoAddMultiple import batch_put
 
+with open("allCourses_test.json") as json_file:
+    courseCatalog = json.load(json_file)['courseCatalog_arr']
+
 def test_batch():
     assert(batch_put(courseCatalog) == "Adding course: CSE1000")
-
-if __name__ == '__main__':
-    with open("allCourses_test.json") as json_file:
-        courseCatalog = json.load(json_file)['courseCatalog_arr']
-    batch_put(courseCatalog)
-
-if __name__ == '__main__':
-    with open("allCourses_test.json") as json_file:
-        courseCatalog = json.load(json_file)['courseCatalog_arr']
-    batch_put(courseCatalog)
