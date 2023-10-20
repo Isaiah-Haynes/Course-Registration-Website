d5c02495e356c379ca4138a68395cc434ed6790d
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2023-10-19 20:58:10 -0400

    adding multiple views

---
+++ src/views/AdminHomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <ul class="nav-links">
+      <li>
+        <RouterLink to="/">Log out</RouterLink>
+      </li>
+    </ul>
+  </nav>
+    <main class="home">
+      <h2>Admin Home Page</h2>
+      <p>
+        This is the admin home page, I assume most, if not all, admin related things can be accomplished
+        from this one page
+      </p>
+    </main>
+  </template>
+  
+  <style>
+  .home {
+    padding: 1rem;
+  }
+  
+  .home h2 {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+  
+  .home p {
+    margin-bottom: 1rem;
+  }
+  </style>
+  
