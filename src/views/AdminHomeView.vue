5485bf6c3532e9738acb3611c551ea8c4ff637ab
Kaitlyn Ha <kaitlynha@Kaitlyns-MacBook-Pro.local>
2023-11-25 18:24:04 -0500

    add adming pages

--- src/views/AdminHomeView.vue
+++ src/views/AdminHomeView.vue
@@ -1,32 +1,101 @@
 <template>
-  <nav>
-    <ul class="nav-links">
-      <li>
-        <RouterLink to="/">Log out</RouterLink>
-      </li>
-    </ul>
-  </nav>
-    <main class="home">
-      <h2>Admin Home Page</h2>
-      <p>
-        This is the admin home page, I assume most, if not all, admin related things can be accomplished
-        from this one page
-      </p>
-    </main>
-  </template>
-  
-  <style>
+  <div class="title-bar">
+    <h2>Admin Home Page</h2>
+    <RouterLink to="/">Log out</RouterLink>
+  </div>
+  <main class="home">
+    <div class="cards">
+ 
+      <div class="card-row">
+        <p class="card-title">Manage Students</p>
+        <div class="actions">
+          <p>Add Student</p>
+          <p>Edit Student</p>
+          <p>Remove Student</p>
+        </div>
+      </div>
+ 
+      <div class="card-row">
+        <p class="card-title">Manage Professors</p>
+        <div class="actions">
+          <p>Add Professor</p>
+          <p>Edit Professor</p>
+          <p>Remove Professor</p>
+        </div>
+      </div>
+
+      <div class="card-row">
+        <p class="card-title">Manage Courses</p>
+        <div class="actions">
+          <p>Add Course</p>
+          <p>Edit Course</p>
+          <p>Remove Course</p>
+        </div>
+      </div>
+    </div>
+  </main>
+ </template>
+
+
+<style>
   .home {
     padding: 1rem;
   }
-  
-  .home h2 {
+   .home h2 {
     font-size: 1.5rem;
     margin-bottom: 2rem;
   }
-  
-  .home p {
+   .home p {
     margin-bottom: 1rem;
   }
+ 
+ 
+  .title-bar {
+    display: flex;
+    justify-content: space-between;
+    margin: 2rem;
+  }
+ 
+ 
+  .cards {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+  }
+ 
+ 
+  .card-row {
+    background-color: white;
+    display: flex;
+  }
+ 
+ 
+  .card-title {
+    background-color: #b3caf4;
+    width: 16%;
+    height: 12rem;
+    border-radius: 0.4rem;
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    -webkit-text-fill-color: #f4f8ff;
+  }
+ 
+ 
+  .actions {
+    background-color: #f4f8ff;
+    width: 82%;
+    height: 12rem;
+    border-radius: 0.4rem;
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    -webkit-text-fill-color: #b3caf4;
+  }
+ 
+ 
   </style>
-  
